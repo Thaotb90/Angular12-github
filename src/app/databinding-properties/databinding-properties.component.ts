7ba0234f1264deb6fdadb2bbbf233f15f2278538
Thaotb90 <91739818+Thaotb90@users.noreply.github.com>
2021-10-03 22:09:39 +0700

    yes

--- src/app/databinding-properties/databinding-properties.component.ts
+++ src/app/databinding-properties/databinding-properties.component.ts
@@ -3,20 +3,30 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-databinding-properties',
   templateUrl: './databinding-properties.component.html',
-  styleUrls: ['./databinding-properties.component.scss']
+  styleUrls: ['./databinding-properties.component.scss'],
 })
 export class DatabindingPropertiesComponent implements OnInit {
-public name = "Trương An Huy";
-public name2 = "Trương Thế Hưng";
-public age = 20;
+  public name = 'Trương An Huy';
+  public name2 = 'Trương Thế Hưng';
+  public age = 20;
 
-public resetname():void {
- console.log('resetname');
- this.name = ''; 
-}
-  constructor() { }
-
-  ngOnInit(): void {
+  public resetname(): void {
+    console.log('resetname');
+    this.name = '';
   }
+  public traiCay = ['Táo', 'Xoài', 'Dưa', 'Bưởi', 'Cam'];
 
+  public traiCay2 = [
+    { ten: 'Mận', gia: 12 },
+    { ten: 'Ổi', gia: 32 },
+    { ten: 'Mơ', gia: 22 },
+    { ten: 'Đào', gia: -12 },
+    { ten: 'Mít', gia: 42 },
+  ];
+
+  constructor() {}
+
+  public ngOnInit(): void {
+    console.log('trai cay =', this.traiCay);
+  }
 }
