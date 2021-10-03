305c0abda7ab0bf42f29dc27b5c2f985a7a4661f
Thaotb90 <91739818+Thaotb90@users.noreply.github.com>
2021-10-03 19:48:51 +0700

    yes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -14,6 +14,7 @@ import { DatabindingPropertiesComponent } from './databinding-properties/databin
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
--- src/app/databinding-properties/databinding-properties.component.ts
+++ src/app/databinding-properties/databinding-properties.component.ts
@@ -6,7 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./databinding-properties.component.scss']
 })
 export class DatabindingPropertiesComponent implements OnInit {
+public name = "Trương An Huy";
+public name2 = "Trương Thế Hưng";
+public age = 20;
 
+public resetname():void {
+ console.log('resetname');
+ this.name = ''; 
+}
   constructor() { }
 
   ngOnInit(): void {
