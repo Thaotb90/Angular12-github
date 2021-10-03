889e41282c1d7b91efe3b658518d17175779a124
Thaotb90 <91739818+Thaotb90@users.noreply.github.com>
2021-10-03 19:48:42 +0700

    yes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,13 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-public appName = 'Trương An Huy';
   constructor() { }
 
   ngOnInit(): void {
   }
 
-}
-export class HomeComponent {
-  
 }
